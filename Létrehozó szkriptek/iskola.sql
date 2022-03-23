c005705badb9d9392a4df01e521b3894771924d0
Sajti Martin <sajti.martin@gmail.com>
2022-03-23 23:00:15 +0100

    egyesített db szkript létrehozva
    kell még a kapcsolat és az integritás ellenőrzés beállítása

--- Létrehozó szkriptek/iskola.sql
+++ Létrehozó szkriptek/iskola.sql
@@ -1,21 +1,21 @@
-DROP TABLE IF EXISTS mytable;
-CREATE TABLE mytable(
+DROP TABLE IF EXISTS iskola;
+CREATE TABLE iskola(
    id                INTEGER  NOT NULL PRIMARY KEY 
   ,nev               VARCHAR(50) NOT NULL
   ,varhato_befejezes INTEGER  NOT NULL
 );
-INSERT INTO mytable(id,nev,varhato_befejezes) VALUES (1,'Állatorvostudományi Egyetem',2024);
-INSERT INTO mytable(id,nev,varhato_befejezes) VALUES (2,'Bolyai János Gimnázium',2023);
-INSERT INTO mytable(id,nev,varhato_befejezes) VALUES (3,'Kandó Kálmán Szakközépiskola',2025);
-INSERT INTO mytable(id,nev,varhato_befejezes) VALUES (4,'Állatorvostudományi Egyetem',2026);
-INSERT INTO mytable(id,nev,varhato_befejezes) VALUES (5,'Szegedi Tudományegyetem',2023);
-INSERT INTO mytable(id,nev,varhato_befejezes) VALUES (6,'Szegedi Tudományegyetem',2026);
-INSERT INTO mytable(id,nev,varhato_befejezes) VALUES (7,'Bolyai János Gimnázium',2024);
-INSERT INTO mytable(id,nev,varhato_befejezes) VALUES (8,'Szegedi Tudományegyetem',2022);
-INSERT INTO mytable(id,nev,varhato_befejezes) VALUES (9,'Kandó Kálmán Szakközépiskola',2023);
-INSERT INTO mytable(id,nev,varhato_befejezes) VALUES (10,'Állatorvostudományi Egyetem',2027);
-INSERT INTO mytable(id,nev,varhato_befejezes) VALUES (11,'Állatorvostudományi Egyetem',2022);
-INSERT INTO mytable(id,nev,varhato_befejezes) VALUES (12,'Bolyai János Gimnázium',2024);
-INSERT INTO mytable(id,nev,varhato_befejezes) VALUES (13,'Szegedi Tudományegyetem',2022);
-INSERT INTO mytable(id,nev,varhato_befejezes) VALUES (14,'Kandó Kálmán Szakközépiskola',2025);
-INSERT INTO mytable(id,nev,varhato_befejezes) VALUES (15,'Állatorvostudományi Egyetem',2026);
+INSERT INTO iskola(id,nev,varhato_befejezes) VALUES (1,'Állatorvostudományi Egyetem',2024);
+INSERT INTO iskola(id,nev,varhato_befejezes) VALUES (2,'Bolyai János Gimnázium',2023);
+INSERT INTO iskola(id,nev,varhato_befejezes) VALUES (3,'Kandó Kálmán Szakközépiskola',2025);
+INSERT INTO iskola(id,nev,varhato_befejezes) VALUES (4,'Állatorvostudományi Egyetem',2026);
+INSERT INTO iskola(id,nev,varhato_befejezes) VALUES (5,'Szegedi Tudományegyetem',2023);
+INSERT INTO iskola(id,nev,varhato_befejezes) VALUES (6,'Szegedi Tudományegyetem',2026);
+INSERT INTO iskola(id,nev,varhato_befejezes) VALUES (7,'Bolyai János Gimnázium',2024);
+INSERT INTO iskola(id,nev,varhato_befejezes) VALUES (8,'Szegedi Tudományegyetem',2022);
+INSERT INTO iskola(id,nev,varhato_befejezes) VALUES (9,'Kandó Kálmán Szakközépiskola',2023);
+INSERT INTO iskola(id,nev,varhato_befejezes) VALUES (10,'Állatorvostudományi Egyetem',2027);
+INSERT INTO iskola(id,nev,varhato_befejezes) VALUES (11,'Állatorvostudományi Egyetem',2022);
+INSERT INTO iskola(id,nev,varhato_befejezes) VALUES (12,'Bolyai János Gimnázium',2024);
+INSERT INTO iskola(id,nev,varhato_befejezes) VALUES (13,'Szegedi Tudományegyetem',2022);
+INSERT INTO iskola(id,nev,varhato_befejezes) VALUES (14,'Kandó Kálmán Szakközépiskola',2025);
+INSERT INTO iskola(id,nev,varhato_befejezes) VALUES (15,'Állatorvostudományi Egyetem',2026);
--- Létrehozó szkriptek/kapcsolat.sql
+++ Létrehozó szkriptek/kapcsolat.sql
@@ -1,31 +1,31 @@
-CREATE TABLE mytable(
+CREATE TABLE kapcsolat(
    ID          INTEGER  NOT NULL PRIMARY KEY 
   ,Név         VARCHAR(15) NOT NULL
   ,Emailcím    VARCHAR(24) NOT NULL
   ,Telefonszám INTEGER  NOT NULL
 );
-INSERT INTO mytable(ID,Név,Emailcím,Telefonszám) VALUES (1,'Gáspár Mihály','gasparmihaly@gmail.com',0620516675);
-INSERT INTO mytable(ID,Név,Emailcím,Telefonszám) VALUES (2,'Veres Kálmán','vereskalman@gmail.com',06202649994);
-INSERT INTO mytable(ID,Név,Emailcím,Telefonszám) VALUES (3,'Kelemen Ferenc','kelemenferenc@gmail.com',06201604695);
-INSERT INTO mytable(ID,Név,Emailcím,Telefonszám) VALUES (4,'Fülöp Rajmund','fuloprajmund@gmail.com',06304753949);
-INSERT INTO mytable(ID,Név,Emailcím,Telefonszám) VALUES (5,'Pataki Bálint','patakibalint@gmail.com',06309066828);
-INSERT INTO mytable(ID,Név,Emailcím,Telefonszám) VALUES (6,'Tamás Márkó','tamasmarko@gmail.com',06309218250);
-INSERT INTO mytable(ID,Név,Emailcím,Telefonszám) VALUES (7,'Molnár István','molnaristvan@gmail.com',6302078039);
-INSERT INTO mytable(ID,Név,Emailcím,Telefonszám) VALUES (8,'Faragó Levente','faragolevente@gmail.com',061481971);
-INSERT INTO mytable(ID,Név,Emailcím,Telefonszám) VALUES (9,'Fehér Ákos','feherakos@gmail.com',06708856138);
-INSERT INTO mytable(ID,Név,Emailcím,Telefonszám) VALUES (10,'Balázs Károly','balazskaroly@gmail.com',06706989492);
-INSERT INTO mytable(ID,Név,Emailcím,Telefonszám) VALUES (11,'Székely Flórián','szekelyflorian@gmail.com',0670574882);
-INSERT INTO mytable(ID,Név,Emailcím,Telefonszám) VALUES (12,'Vass Ádám','vassadam@gmail.com',06705772249);
-INSERT INTO mytable(ID,Név,Emailcím,Telefonszám) VALUES (13,'Takács Gergő','takacsgergo@gmail.com',06709270015);
-INSERT INTO mytable(ID,Név,Emailcím,Telefonszám) VALUES (14,'Molnár Dalma','molnardalma@gmail.com',06203598815);
-INSERT INTO mytable(ID,Név,Emailcím,Telefonszám) VALUES (15,'Novák Lilla','novaklilla@gmail.com',06206235157);
-INSERT INTO mytable(ID,Név,Emailcím,Telefonszám) VALUES (16,'Kis Georgina','kisgeorgina@gmail.com',06202595982);
-INSERT INTO mytable(ID,Név,Emailcím,Telefonszám) VALUES (17,'Biró Dorka','birodorka@gmail.com',062091156);
-INSERT INTO mytable(ID,Név,Emailcím,Telefonszám) VALUES (18,'Takács Réka','takacsreka@gmail.com',06301539788);
-INSERT INTO mytable(ID,Név,Emailcím,Telefonszám) VALUES (19,'Fülöp Regina','fulopregina@gmail.com',06301192779);
-INSERT INTO mytable(ID,Név,Emailcím,Telefonszám) VALUES (20,'Gáspár Ágnes','gasparagnes@gmail.com',06302852781);
-INSERT INTO mytable(ID,Név,Emailcím,Telefonszám) VALUES (21,'Jakab Dominika','jakabdominika@gmail.com',06305355355);
-INSERT INTO mytable(ID,Név,Emailcím,Telefonszám) VALUES (22,'Egyed Kíra','egyedkira@gmail.com',06705656529);
-INSERT INTO mytable(ID,Név,Emailcím,Telefonszám) VALUES (23,'Pintér Éva','pintereva@gmail.com',06706718125);
-INSERT INTO mytable(ID,Név,Emailcím,Telefonszám) VALUES (24,'Vincze Sára','vinczeSara@gmail.com',06704373270);
-INSERT INTO mytable(ID,Név,Emailcím,Telefonszám) VALUES (25,'Halász Fruzsina','halaszfruzsina@gmail.com',06701544194);
+INSERT INTO kapcsolat(ID,Név,Emailcím,Telefonszám) VALUES (1,'Gáspár Mihály','gasparmihaly@gmail.com',0620516675);
+INSERT INTO kapcsolat(ID,Név,Emailcím,Telefonszám) VALUES (2,'Veres Kálmán','vereskalman@gmail.com',06202649994);
+INSERT INTO kapcsolat(ID,Név,Emailcím,Telefonszám) VALUES (3,'Kelemen Ferenc','kelemenferenc@gmail.com',06201604695);
+INSERT INTO kapcsolat(ID,Név,Emailcím,Telefonszám) VALUES (4,'Fülöp Rajmund','fuloprajmund@gmail.com',06304753949);
+INSERT INTO kapcsolat(ID,Név,Emailcím,Telefonszám) VALUES (5,'Pataki Bálint','patakibalint@gmail.com',06309066828);
+INSERT INTO kapcsolat(ID,Név,Emailcím,Telefonszám) VALUES (6,'Tamás Márkó','tamasmarko@gmail.com',06309218250);
+INSERT INTO kapcsolat(ID,Név,Emailcím,Telefonszám) VALUES (7,'Molnár István','molnaristvan@gmail.com',6302078039);
+INSERT INTO kapcsolat(ID,Név,Emailcím,Telefonszám) VALUES (8,'Faragó Levente','faragolevente@gmail.com',061481971);
+INSERT INTO kapcsolat(ID,Név,Emailcím,Telefonszám) VALUES (9,'Fehér Ákos','feherakos@gmail.com',06708856138);
+INSERT INTO kapcsolat(ID,Név,Emailcím,Telefonszám) VALUES (10,'Balázs Károly','balazskaroly@gmail.com',06706989492);
+INSERT INTO kapcsolat(ID,Név,Emailcím,Telefonszám) VALUES (11,'Székely Flórián','szekelyflorian@gmail.com',0670574882);
+INSERT INTO kapcsolat(ID,Név,Emailcím,Telefonszám) VALUES (12,'Vass Ádám','vassadam@gmail.com',06705772249);
+INSERT INTO kapcsolat(ID,Név,Emailcím,Telefonszám) VALUES (13,'Takács Gergő','takacsgergo@gmail.com',06709270015);
+INSERT INTO kapcsolat(ID,Név,Emailcím,Telefonszám) VALUES (14,'Molnár Dalma','molnardalma@gmail.com',06203598815);
+INSERT INTO kapcsolat(ID,Név,Emailcím,Telefonszám) VALUES (15,'Novák Lilla','novaklilla@gmail.com',06206235157);
+INSERT INTO kapcsolat(ID,Név,Emailcím,Telefonszám) VALUES (16,'Kis Georgina','kisgeorgina@gmail.com',06202595982);
+INSERT INTO kapcsolat(ID,Név,Emailcím,Telefonszám) VALUES (17,'Biró Dorka','birodorka@gmail.com',062091156);
+INSERT INTO kapcsolat(ID,Név,Emailcím,Telefonszám) VALUES (18,'Takács Réka','takacsreka@gmail.com',06301539788);
+INSERT INTO kapcsolat(ID,Név,Emailcím,Telefonszám) VALUES (19,'Fülöp Regina','fulopregina@gmail.com',06301192779);
+INSERT INTO kapcsolat(ID,Név,Emailcím,Telefonszám) VALUES (20,'Gáspár Ágnes','gasparagnes@gmail.com',06302852781);
+INSERT INTO kapcsolat(ID,Név,Emailcím,Telefonszám) VALUES (21,'Jakab Dominika','jakabdominika@gmail.com',06305355355);
+INSERT INTO kapcsolat(ID,Név,Emailcím,Telefonszám) VALUES (22,'Egyed Kíra','egyedkira@gmail.com',06705656529);
+INSERT INTO kapcsolat(ID,Név,Emailcím,Telefonszám) VALUES (23,'Pintér Éva','pintereva@gmail.com',06706718125);
+INSERT INTO kapcsolat(ID,Név,Emailcím,Telefonszám) VALUES (24,'Vincze Sára','vinczeSara@gmail.com',06704373270);
+INSERT INTO kapcsolat(ID,Név,Emailcím,Telefonszám) VALUES (25,'Halász Fruzsina','halaszfruzsina@gmail.com',06701544194);
