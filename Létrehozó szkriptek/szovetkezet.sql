8d2f8a433d6211e375f3d3d84f33b1da203befd5
krisz <belovai.k22@gmail.com>
2022-03-23 13:56:29 +0100

    adatb létrehozó scriptek

---
+++ Létrehozó szkriptek/szovetkezet.sql
@@ -0,0 +1,9 @@
+CREATE TABLE mytable(
+   ID  INTEGER  NOT NULL PRIMARY KEY 
+  ,Név VARCHAR(23) NOT NULL
+);
+INSERT INTO mytable(ID,Név) VALUES (1,'Diák-Meló Szövetkezet');
+INSERT INTO mytable(ID,Név) VALUES (2,'Diák-Mind Szövetkezet');
+INSERT INTO mytable(ID,Név) VALUES (3,'Fürgeláb Szövetkezet');
+INSERT INTO mytable(ID,Név) VALUES (4,'Dolgos Diák Szövetkezet');
+INSERT INTO mytable(ID,Név) VALUES (5,'DiákJobs Szövetkezet');
